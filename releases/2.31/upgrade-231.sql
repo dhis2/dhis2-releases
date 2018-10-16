837ea900ef3122d6e6c406aef1e9df1943aa615a
David Katuscak <david@dhis2.org>
2018-10-16 15:00:28 +0200

    Upgrade script for converting trackedentitydatavalue table into jsonb column of programstageinstance table - DHIS2-4477

---
+++ releases/2.31/upgrade-231.sql
@@ -0,0 +1,7 @@
+------------------------------------------------------------------------------------------------------------------
+---------- SQL commands converting trackedentitydatavalue into new json column in programstageinstance  ----------
+------------------------------------------------------------------------------------------------------------------
+
+alter table programstageinstance add column eventdatavalues jsonb;
+
+update programstageinstance psi set eventdatavalues = (select to_jsonb(array_agg(jsonb_build_object('dataElement', de.uid, 'created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby ))) from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid where tedv.programstageinstanceid = psi.programstageinstanceid);
