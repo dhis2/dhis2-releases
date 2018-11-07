54758cd0c0a43f0344635c20abaace0a61a3df20
David Katuscak <david@dhis2.org>
2018-11-07 15:43:58 +0100

    Generates jsonb object with dataElement UIDs as keys for dataValue objects - DHIS2-4477

--- releases/2.31/upgrade-231.sql
+++ releases/2.31/upgrade-231.sql
@@ -4,6 +4,7 @@
 
 alter table programstageinstance add column eventdatavalues jsonb;
 
+-- update programstageinstance psi set eventdatavalues = (select to_jsonb(array_agg(jsonb_build_object('dataElement', de.uid, 'created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby ))) from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid where tedv.programstageinstanceid = psi.programstageinstanceid);
 update programstageinstance psi set eventdatavalues = (select to_jsonb(array_agg(jsonb_build_object('dataElement', de.uid, 'created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby ))) from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid where tedv.programstageinstanceid = psi.programstageinstanceid);
 
 drop table trackedentitydatavalue;
