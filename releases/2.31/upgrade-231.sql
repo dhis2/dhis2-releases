7c6b2639969bef27c345693789c91a631e876e08
David Katuscak <david@dhis2.org>
2019-01-02 09:43:40 +0100

    WIP: Updated upgrade script. Should NOT be merged. Used just to have a backup. Will be rewritten into Flyway upgrade script, respectively some commands will be run directly on Demo DB - DHIS2-4477

--- releases/2.31/upgrade-231.sql
+++ releases/2.31/upgrade-231.sql
@@ -4,19 +4,36 @@
 
 alter table programstageinstance add column eventdatavalues jsonb;
 
--- update programstageinstance psi set eventdatavalues = (select to_jsonb(array_agg(jsonb_build_object('dataElement', de.uid, 'created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby ))) from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid where tedv.programstageinstanceid = psi.programstageinstanceid);
--- update programstageinstance psi set eventdatavalues = (select to_jsonb(array_agg(jsonb_build_object ( de.uid , jsonb_build_object('created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby )))) from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid where tedv.programstageinstanceid = psi.programstageinstanceid);
+######################################################
+-- clean data in Demo DB a bit:
+
+-- corrects invalid values of datavalues in Demo DB
+update datavalue set value = regexp_replace(value, '^-(\d+)', '\1') where value ~ '^-\d+';
+
+-- corrects invalid values of datavalues in Demo DB
+update datavalue set value = regexp_replace(value, '^0$', '1') where value ~ '^0$';
+
+-- Fixes the domaintype in Demo DB - should be run only on Demo DB! (Requested by Juan Manuel)
+update dataelement set domaintype = 'TRACKER' where dataelementid = 423669;
+
+######################################################
+
+-- Required for jsonb for events issue:
+
+-- Fixes invalid timestamps in DB (problems found in Demo DB, but can be run on all DBs)
+update trackedentitydatavalue set created = substring( created::text, 1, 23 )::timestamp where created::text ~ '.*\.\d{6}$';
+update trackedentitydatavalue set lastupdated = substring( lastupdated::text, 1, 23 )::timestamp where lastupdated::text ~ '.*\.\d{6}$';
 
 -- Fetch data from trackedentitydatavalue table, process them, creates a jsonb representation of them and stores into eventdatavalues column 
 -- in programstageinstance table. 
 -- Various PostgreSql functions are used to get the required format of the JSON string. The string is then cast to jsonb type. 
 -- Takes care of empty values. When there is no trackedentitydatavalue for given programstageinstance a null is saved. This can be changed 
 -- and specified JSON string can be saved instead (change NULL for the given JSON String).
-UPDATE programstageinstance psi SET eventdatavalues =
+update programstageinstance psi set eventdatavalues =
   to_jsonb(coalesce(nullif(replace(array_to_string(array(
-    SELECT jsonb_build_object(de.uid, jsonb_build_object('created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 
+    select jsonb_build_object(de.uid, jsonb_build_object('created', tedv.created, 'lastUpdated', tedv.lastupdated, 'value', tedv.value, 
                                                          'providedElsewhere', tedv.providedelsewhere, 'storedBy', tedv.storedby)) 
-    FROM trackedentitydatavalue tedv JOIN dataelement de ON tedv.dataelementid = de.dataelementid
-    WHERE tedv.programstageinstanceid = psi.programstageinstanceid), ','), '}},{', '},'), ''), NULL)::json);
+    from trackedentitydatavalue tedv join dataelement de on tedv.dataelementid = de.dataelementid
+    where tedv.programstageinstanceid = psi.programstageinstanceid), ','), '}},{', '},'), ''), NULL)::json);
 
 drop table trackedentitydatavalue;
