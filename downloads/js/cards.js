17864bcfadbe98cbaf3f4786da045950e9668178
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2024-06-06 05:32:31 +0200

    Update cards.js

--- downloads/js/cards.js
+++ downloads/js/cards.js
@@ -44,6 +44,12 @@ function createReleaseCard(release, patchVersion) {
     // Create a div with the class 'download-card'
     const cardElement = createElement('div', 'download-card');
     // Set the inner HTML of the card with the release and patch version details
+    
+    // change release.releaseDate and patchVersion.releaseDate to human readable date without the day of the week
+
+    release.releaseDate = new Date(release.releaseDate).toLocaleDateString('en-UK', { year: 'numeric', month: 'long', day: 'numeric' });
+    patchVersion.releaseDate = new Date(patchVersion.releaseDate).toLocaleDateString('en-UK', { year: 'numeric', month: 'long', day: 'numeric' });
+
     cardElement.innerHTML = `
         <div class="dc-title">
             <span class="dc-title-version">DHIS2 version ${patchVersion.displayName || patchVersion.name}</span>
@@ -51,6 +57,7 @@ function createReleaseCard(release, patchVersion) {
             ${(patchVersion.hotfix || patchVersion.hotfixVersion) ? '<span class="dc-badge dc-badge-hotfix"><i class="svg-icon svg-icon-hotfix"><!-- SVG hotfix --></i>Hotfix</span>' : ''}
         </div>
         <div class="dc-subtitle">
+
             <span>Latest patch ${patchVersion.releaseDate}</span>
             <span>Released ${release.releaseDate}</span>
         </div>
